48952631abff2bf46b59a64e10100047eead451a
ikamrava <ikamrava>
2023-01-29 16:42:37 +0000

    searchlink

--- app/SearchBox.tsx
+++ app/SearchBox.tsx
@@ -1,9 +1,21 @@
 "use client"
 
-import { useState } from "react"
+import { useRouter } from "next/navigation"
+import { FormEvent, useState } from "react"
 
 function SearchBox() {
     const [input,setInput] = useState("")
+    const router = useRouter()
+    
+    const submitHandler = (e:FormEvent<HTMLElement>)=>{
+        
+        
+        e.preventDefault()
+        if(!input) return
+        router.push(`/search?term=${input}`)
+
+
+    }
   return (
     <form action="" className="max-w-6xl mx-auto flex justify-between items-center px-5">
         <input placeholder="Searck Keywords..." type="text"
@@ -14,6 +26,7 @@ function SearchBox() {
         <button type='submit' 
         className=" text-orange-400 disabled:text-gray-400"
         disabled={!input}
+        onSubmit={submitHandler}
         >Search</button>
     </form>
   )
