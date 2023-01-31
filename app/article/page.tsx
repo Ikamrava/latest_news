2b0012bb997b806701fa1dac1d468f30d07942a9
ikamrava <ikamrava>
2023-01-31 16:23:20 +0000

    add if on info page

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -1,3 +1,4 @@
+import Loading from "../Loading"
 
 
 type Props = {
@@ -8,27 +9,26 @@ function page(props:Props) {
    const {searchParams} = props
 
    
-  
-  return (
-    
+  if(searchParams){
+return (
     <article>
       <section className="flex flex-col lg:flex-row pb-24 px-0 lg:px-10">
-        {searchParams && searchParams.image && (
+        {searchParams.image && (
           <img src={searchParams.image}
           className="h-50 max-w-md mx-auto md:max-w-lg lg:max-w-xl object-cover rounded-lg shadow-md"
           />
         )}
         <div className="px-10">
           <h1 className=" headerTitle px-0 no-underline pb-2">
-            {searchParams && searchParams.title}
+            {searchParams.title}
           </h1>
           <div className=" flex flex-col md:flex-row divide-x-2 space-x-4">
             <h2 className="font-bold">By: 
-              {searchParams && searchParams.author}
+              {searchParams.author}
             </h2>
-            <h2 className="font-bold pl-4">Source: {searchParams && searchParams.source}</h2>
+            <h2 className="font-bold pl-4">Source: {searchParams.source}</h2>
             <p className="pl-4">
-              {searchParams && searchParams.published_at}
+              {searchParams.published_at}
               </p>
           </div>
           <p className=" pt-4">{searchParams && searchParams.description}</p>
@@ -36,6 +36,13 @@ function page(props:Props) {
       </section>
     </article>
   )
+  }else{
+    return(
+      <h1>There is no data</h1>
+    )
+  }
+  
+  
 }
 
 export default page
