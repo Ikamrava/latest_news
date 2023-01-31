f423993545ed8e89dd2f5faa281b255604d4d161
ikamrava <ikamrava>
2023-01-31 15:42:27 +0000

    build pronblem has been fixed

--- app/article/page-old.tsx
+++ app/article/page-old.tsx
@@ -1,40 +1,40 @@
+import { notFound } from "next/navigation"
 
 
 type Props = {
-    searchParams?:Article
+    searchParams:Article
 }
 
-function page(props:Props) {
-   const {searchParams} = props
-   
+function Article({searchParams}:Props) {
+
+  const article:Article = searchParams
   
   return (
-    
     <article>
       <section className="flex flex-col lg:flex-row pb-24 px-0 lg:px-10">
-        {searchParams && searchParams.image && (
-          <img src={searchParams.image}
+        {article.image && (
+          <img src={article.image}
           className="h-50 max-w-md mx-auto md:max-w-lg lg:max-w-xl object-cover rounded-lg shadow-md"
           />
         )}
         <div className="px-10">
           <h1 className=" headerTitle px-0 no-underline pb-2">
-            {searchParams && searchParams.title}
+            {article.title}
           </h1>
           <div className=" flex flex-col md:flex-row divide-x-2 space-x-4">
             <h2 className="font-bold">By: 
-              {searchParams && searchParams.author}
+              {article.author}
             </h2>
-            <h2 className="font-bold pl-4">Source: {searchParams && searchParams.source}</h2>
+            <h2 className="font-bold pl-4">Source: {article.source}</h2>
             <p className="pl-4">
-              {searchParams && searchParams.published_at}
+              {article.published_at}
               </p>
           </div>
-          <p className=" pt-4">{searchParams && searchParams.description}</p>
+          <p className=" pt-4">{article.description}</p>
         </div>
       </section>
     </article>
   )
 }
 
-export default page
+export default Article
