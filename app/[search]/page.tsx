2299f2f9d6fdc9548b600c6ace95bb2fc5251551
ikamrava <ikamrava>
2023-01-31 21:11:44 +0000

    dynamic

--- app/[search]/page.tsx
+++ app/[search]/page.tsx
@@ -2,9 +2,13 @@ import fetchNews from "../../lib/fetchNews"
 import NewsList from "../NewsList"
 
 
+
 type Props = {
     searchParams?:{term:string}
+
 }
+
+export const dynamic='force-dynamic';
 
 async function SearchPage({searchParams}:Props) {
     const news:NewsResponse = await fetchNews(
